bfd169c8629021107f3e9912addab4b0b9e848fe
arnavbhatiamait <arnavbhatiamait@gmail.com>
2026-04-30 14:07:12 +0530

    updated agents for executing full workflow

--- 15_Types_of_Agents/Debugging/agent.py
+++ 15_Types_of_Agents/Debugging/agent.py
@@ -1,9 +1,11 @@
 from typing import Annotated
 from typing_extensions import TypedDict
+
 # from langchain_openai import ChatOpenAI
 from langgraph.graph import END, START
 from langgraph.graph.state import StateGraph
 from langgraph.graph.message import add_messages
+
 # from langgraph.prebuilt.tool_node import ToolNode
 from langgraph.prebuilt import ToolNode
 from langchain_core.tools import tool
@@ -12,27 +14,34 @@ import os
 from langchain_ollama import ChatOllama
 from langchain_groq import ChatGroq
 from dotenv import load_dotenv
+
 # from langgraph.prebuilt import ToolExecutor
 load_dotenv()
 
-os.environ["OPENAI_API_KEY"]=os.getenv("OPENAI_API_KEY")
+os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
 
-os.environ["LANGSMITH_API_KEY"]=os.getenv("LANGCHAIN_API_KEY")
+os.environ["LANGSMITH_API_KEY"] = os.getenv("LANGCHAIN_API_KEY")
+
 
 class State(TypedDict):
-    messages:Annotated[list[BaseMessage],add_messages]
-llm=ChatOllama(model="llama3.2:latest",verbose=1)
+    messages: Annotated[list[BaseMessage], add_messages]
+
+
+llm = ChatOllama(model="llama3.2:latest", verbose=1)
 
 
 def make_default_graph():
-    graph_workflow=StateGraph(State)
+    graph_workflow = StateGraph(State)
+
     def call_model(state):
-        return{"messages":[llm.invoke(state['messages'])]}
-    graph_workflow.add_node("agent",call_model)
-    graph_workflow.add_edge(START,"agent")
-    graph_workflow.add_edge("agent",END)
-    agent=graph_workflow.compile()
+        return {"messages": [llm.invoke(state["messages"])]}
+
+    graph_workflow.add_node("agent", call_model)
+    graph_workflow.add_edge(START, "agent")
+    graph_workflow.add_edge("agent", END)
+    agent = graph_workflow.compile()
     return agent
+
 
 def make_alternative_graph():
     """Make a tool-calling agent"""
@@ -45,6 +54,7 @@ def make_alternative_graph():
     tool_node = ToolNode([add])
     # tool_node=ToolExecutor(tool=[add])
     model_with_tools = llm.bind_tools([add])
+
     def call_model(state):
         return {"messages": [model_with_tools.invoke(state["messages"])]}
 
@@ -61,10 +71,13 @@ def make_alternative_graph():
     graph_workflow.add_node("tools", tool_node)
     graph_workflow.add_edge("tools", "agent")
     graph_workflow.add_edge(START, "agent")
-    graph_workflow.add_conditional_edges("agent", should_continue,{ "tools": "tools", END: END })
+    graph_workflow.add_conditional_edges(
+        "agent", should_continue, {"tools": "tools", END: END}
+    )
 
     agent = graph_workflow.compile()
     return agent
 
+
 # agent=make_default_graph()
-agent=make_alternative_graph()
+agent = make_alternative_graph()
